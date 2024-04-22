13587928ac9a7d90d4f003837150ba4a028f9ebe
sch8402 <82961569+sch8402@users.noreply.github.com>
2024-04-22 22:43:06 +0900

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://52.79.237.43/msrgirin/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://52.79.237.43/foserver/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
